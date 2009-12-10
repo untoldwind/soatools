d7d5bcd3adbd7f9b8b9c7032f4e00d087a069d80
Untoldwind <landru@untoldwind.net>
2009-12-10 12:28:32 +0100

    Fixed minor issues

--- logging/logstore-ws/src/main/java/de/objectcode/soatools/logstore/ws/LogStoreLogDetailController.java
+++ logging/logstore-ws/src/main/java/de/objectcode/soatools/logstore/ws/LogStoreLogDetailController.java
@@ -10,37 +10,41 @@ import org.jboss.seam.annotations.Scope;
 
 @Name("logStoreLogDetail")
 @Scope(ScopeType.CONVERSATION)
-public class LogStoreLogDetailController implements Serializable
-{
-  private static final long serialVersionUID = -11013439034283885L;
+public class LogStoreLogDetailController implements Serializable {
+	private static final long serialVersionUID = -11013439034283885L;
 
-  public final static String VIEW_ID = "/secure/logDetail.xhtml";
+	public final static String VIEW_ID = "/secure/logDetail.xhtml";
 
-  @In
-  LogMessageList logMessageList;
+	@In
+	LogMessageList logMessageList;
 
-  String selectedDetailTab;
+	String selectedDetailTab;
+	int currentPage;
 
-  public String select(LogMessageBean logMessage)
-  {
-    logMessageList.setCurrent(logMessage);
+	public String select(LogMessageBean logMessage) {
+		logMessageList.setCurrent(logMessage);
 
-    return VIEW_ID;
-  }
+		return VIEW_ID;
+	}
 
-  public String getSelectedDetailTab()
-  {
-    return selectedDetailTab;
-  }
+	public String getSelectedDetailTab() {
+		return selectedDetailTab;
+	}
 
-  public void setSelectedDetailTab(String selectedDetailTab)
-  {
-    this.selectedDetailTab = selectedDetailTab;
-  }
+	public void setSelectedDetailTab(String selectedDetailTab) {
+		this.selectedDetailTab = selectedDetailTab;
+	}
 
-  @End
-  public String close()
-  {
-    return LogStoreSearchController.VIEW_ID;
-  }
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+
+	@End
+	public String close() {
+		return LogStoreSearchController.VIEW_ID;
+	}
 }
