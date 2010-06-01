b62c76c91e0ef8963ee2340560c03624046a3e10
Untoldwind <landru@untoldwind.net>
2010-06-01 16:14:52 +0200

    Added resend

--- logging/logstore-ui/src/main/java/de/objectcode/soatools/logstore/ws/LogStoreLogDetailController.java
+++ logging/logstore-ui/src/main/java/de/objectcode/soatools/logstore/ws/LogStoreLogDetailController.java
@@ -2,6 +2,8 @@ package de.objectcode.soatools.logstore.ws;
 
 import java.io.Serializable;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hibernate.Session;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.End;
@@ -9,12 +11,18 @@ import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.Transactional;
+import org.jboss.soa.esb.client.ServiceInvoker;
+import org.jboss.soa.esb.message.Message;
+import org.jboss.soa.esb.message.format.MessageFactory;
 
 import de.objectcode.soatools.logstore.persistent.LogMessage;
 
 @Name("logStoreLogDetail")
 @Scope(ScopeType.CONVERSATION)
 public class LogStoreLogDetailController implements Serializable {
+	private final static Log LOG = LogFactory
+	.getLog(LogStoreLogDetailController.class);
+	
 	private static final long serialVersionUID = -11013439034283885L;
 
 	public final static String VIEW_ID = "/secure/logDetail.xhtml";
@@ -41,6 +49,34 @@ public class LogStoreLogDetailController implements Serializable {
 	public String refresh() {
 		logMessageList.refresh(logStoreDatabase);
 
+		return VIEW_ID;
+	}
+	
+	public String resend() {
+		LogMessageDetailBean logMessageBean = logMessageList.getCurrent();
+		
+		if ( logMessageBean != null ) {
+			Message resendMessage = MessageFactory.getInstance().getMessage();
+			
+			for ( NameValuePair entry : logMessageBean.getBodyList()) {
+				resendMessage.getBody().add(entry.getName(), entry.getValue());
+			}
+			for (NameValuePair entry :logMessageBean.getPropertyList()) {
+				resendMessage.getProperties().setProperty(entry.getName(), entry.getValue());
+			}
+			for(NameValuePair entry : logMessageBean.getContextList()) {
+				resendMessage.getContext().setContext(entry.getName(), entry.getValue());
+			}
+			
+			try {
+				ServiceInvoker invoker = new ServiceInvoker(logMessageBean.getServiceCategory(), logMessageBean.getServiceName());
+				
+				invoker.deliverAsync(resendMessage);
+			}
+			catch ( Exception e ) {
+				LOG.error("Exception", e);
+			}
+		}
 		return VIEW_ID;
 	}
 	
