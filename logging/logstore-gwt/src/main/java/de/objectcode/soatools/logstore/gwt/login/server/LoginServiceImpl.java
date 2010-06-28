966d832e250c2d12587db545b8a664999cb84a6c
Untoldwind <landru@untoldwind.net>
2010-06-28 09:03:38 +0200

    Basic client-server communication

---
+++ logging/logstore-gwt/src/main/java/de/objectcode/soatools/logstore/gwt/login/server/LoginServiceImpl.java
@@ -0,0 +1,26 @@
+package de.objectcode.soatools.logstore.gwt.login.server;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import de.objectcode.soatools.logstore.gwt.login.client.LoginService;
+import de.objectcode.soatools.logstore.gwt.utils.server.GwtController;
+
+@Controller
+@RequestMapping( { "/LoginUI/login.service", "/MainUI/login.service" })
+public class LoginServiceImpl extends GwtController implements LoginService {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public boolean login(String userId, String password) {
+		System.out.println(">>> Loging in: " + userId);
+		return true;
+	}
+
+	@Override
+	public void logout() {
+		System.out.println("Logout");
+	}
+
+}
---
+++ logging/logstore-gwt/src/main/java/de/objectcode/soatools/logstore/gwt/main/client/MainPage.java
@@ -0,0 +1,13 @@
+package de.objectcode.soatools.logstore.gwt.main.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class MainPage implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
